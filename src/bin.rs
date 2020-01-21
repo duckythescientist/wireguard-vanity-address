f305b84fe555fbf1fedd671686bb95e800d063de
duckythescientist <seantaylormurphy@gmail.com>
2020-01-21 12:39:21 -0500

    Add basic regex search
    Note: I've never written Rust in my life, so this code could be
    absolute garbage.

--- src/bin.rs
+++ src/bin.rs
@@ -2,19 +2,29 @@ use std::error::Error;
 use std::fmt;
 use std::io::{self, Write};
 use std::time::{Duration, SystemTime};
+use regex::Regex;
 
 use clap::{App, AppSettings, Arg};
 use num_cpus;
 use rayon::prelude::*;
-use wireguard_vanity_lib::trial;
+use wireguard_vanity_lib::trial_regex;
+use wireguard_vanity_lib::trial_substring;
 
-fn estimate_one_trial() -> Duration {
+fn estimate_one_trial(is_regex: bool) -> Duration {
     let prefix = "prefix";
+    let test_re = Regex::new(r"^aoeu\d?[b-k]{2,3}").unwrap();
     let start = SystemTime::now();
     const COUNT: u32 = 100;
-    (0..COUNT).for_each(|_| {
-        trial(&prefix, 0, 10);
-    });
+    if is_regex {
+        (0..COUNT).for_each(|_| {
+            trial_regex(&test_re);
+        });
+    }
+    else {
+        (0..COUNT).for_each(|_| {
+            trial_substring(&prefix, 0, 10);
+        });
+    }
     let elapsed = start.elapsed().unwrap();
     elapsed.checked_div(COUNT).unwrap()
 }
@@ -93,6 +103,12 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .help("NAME must be found within first RANGE chars of pubkey (default: 10)"),
         )
         .arg(
+            Arg::with_name("REGEX")
+                .long("regex")
+                .takes_value(false)
+                .help("NAME is a regex (not a substring)"),
+        )
+        .arg(
             Arg::with_name("NAME")
                 .required(true)
                 .help("string to find near the start of the pubkey"),
@@ -100,6 +116,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .get_matches();
     let prefix = matches.value_of("NAME").unwrap().to_ascii_lowercase();
     let len = prefix.len();
+    let is_regex = matches.is_present("REGEX");
     let end: usize = 44.min(match matches.value_of("RANGE") {
         Some(range) => range.parse()?,
         None => {
@@ -114,34 +131,38 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Err(ParseError(format!("range {} is too short for len={}", end, len)).into());
     }
 
-    let offsets: u64 = 44.min((1 + end - len) as u64);
-    // todo: this is an approximation, offsets=2 != double the chances
-    let mut num = offsets;
-    let mut denom = 1u64;
-    prefix.chars().for_each(|c| {
-        if c.is_ascii_alphabetic() {
-            num *= 2; // letters can match both uppercase and lowercase
-        }
-        denom *= 64; // base64
-    });
-    let trials_per_key = denom / num;
+    let mut trials_per_key = 0;
+    if ! is_regex {
+        let offsets: u64 = 44.min((1 + end - len) as u64);
+        // todo: this is an approximation, offsets=2 != double the chances
+        let mut num = offsets;
+        let mut denom = 1u64;
+        prefix.chars().for_each(|c| {
+            if c.is_ascii_alphabetic() {
+                num *= 2; // letters can match both uppercase and lowercase
+            }
+            denom *= 64; // base64
+        });
+        trials_per_key = denom / num;
 
-    println!(
-        "searching for '{}' in pubkey[0..{}], one of every {} keys should match",
-        &prefix, end, trials_per_key
-    );
+        println!(
+            "searching for '{}' in pubkey[0..{}], one of every {} keys should match",
+            &prefix, end, trials_per_key
+        );
+    }
+
 
     // todo: dividing by num_cpus will overestimate performance when the
     // cores aren't actually distinct (hyperthreading?). My Core-i7 seems to
     // run at half the speed that this predicts.
 
-    if trials_per_key < 2u64.pow(32) {
-        let est = estimate_one_trial();
-        println!(
-            "one trial takes {}, CPU cores available: {}",
-            format_time(duration_to_f64(est)),
-            num_cpus::get()
-        );
+    let est = estimate_one_trial(is_regex);
+    println!(
+        "one trial takes {}, CPU cores available: {}",
+        format_time(duration_to_f64(est)),
+        num_cpus::get()
+    );
+    if ! is_regex && trials_per_key < 2u64.pow(32) {
         let spk = duration_to_f64(
             est // sec/trial on one core
                 .checked_div(num_cpus::get() as u32) // sec/trial with all cores
@@ -159,11 +180,22 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("hit Ctrl-C to stop");
 
-    // 1M trials takes about 10s on my laptop, so let it run for 1000s
-    (0..100_000_000)
-        .into_par_iter()
-        .map(|_| trial(&prefix, 0, end))
-        .filter_map(|r| r)
-        .try_for_each(print)?;
+    if is_regex {
+        let re = Regex::new(&prefix).unwrap();
+        // 1M trials takes about 10s on my laptop, so let it run for 1000s
+        (0..100_000_000)
+            .into_par_iter()
+            .map(|_| trial_regex(&re))
+            .filter_map(|r| r)
+            .try_for_each(print)?;
+    }
+    else {
+        // 1M trials takes about 10s on my laptop, so let it run for 1000s
+        (0..100_000_000)
+            .into_par_iter()
+            .map(|_| trial_substring(&prefix, 0, end))
+            .filter_map(|r| r)
+            .try_for_each(print)?;
+    }
     Ok(())
 }
